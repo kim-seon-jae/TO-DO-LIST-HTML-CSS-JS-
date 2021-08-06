683f52152b0143c56baa71df9216ea29e4a07ed2
kim-seon-jae <quailty12000@gmail.com>
2021-08-06 12:50:22 +0900

    Add 1.data add, 2. Delete All

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 let inputTextEl = document.querySelector('.input_text')
 let inputBtnEl = document.querySelector('.input_btn')
+let inputBtnRemoveEl = document.querySelector('.input_btn_remove')
 
-let outputEl = document.querySelector('.output')
+let outputUlEl = document.querySelector('.output_ul')
+
+console.log(inputBtnEl)
+
+let arr = [];
+// 해야할 목록 담아두는 배열
+
+inputBtnEl.addEventListener('click',function () {
+  arr.push(inputTextEl.value)
+  let outputDataEl = document.createElement('li');
+  outputDataEl.className='data'
+  outputDataEl.textContent=arr[arr.length-1]
+  outputUlEl.appendChild(outputDataEl)
+})
+
+inputBtnRemoveEl.addEventListener('click',function () {
+  arr=[];
+  while (outputUlEl.firstChild) {
+    outputUlEl.removeChild(outputUlEl.firstChild)
+  }
+})
+
 
